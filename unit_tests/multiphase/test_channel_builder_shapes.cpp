ef55982514e46bd189ea71afb8a96970e84bdffd
Michael Kuhn <mkuhn@nlr.gov>
2026-04-21 14:34:32 -0600

    improve unit test

--- unit_tests/multiphase/test_channel_builder_shapes.cpp
+++ unit_tests/multiphase/test_channel_builder_shapes.cpp
@@ -85,75 +85,50 @@ TEST(ChannelBuilderShapes, is_point_within_planes)
 
 TEST(ChannelBuilderShapes, transform_to_local_coordinates)
 {
-    // Define a simple segment from (0, 0, 0) to (10, 0, 0)
-    // (along x-axis)
-    const amrex::Real start_x = 0.0_rt;
-    const amrex::Real start_y = 0.0_rt;
-    const amrex::Real start_z = 0.0_rt;
-    const amrex::Real end_x = 10.0_rt;
-    const amrex::Real end_y = 0.0_rt;
-    const amrex::Real end_z = 0.0_rt;
+    const amrex::Vector<amrex::Real> start{2.0_rt, 3.0_rt, 5.0_rt};
+    amrex::Vector<amrex::Real> end{10.0_rt, 3.0_rt, 5.0_rt};
 
-    // Point at segment start should have local coords near (0, 0, 0)
-    auto local = amr_wind::channelbuilder::transform_to_local_coordinates(
-        0.0_rt, 0.0_rt, 0.0_rt, start_x, start_y, start_z, end_x, end_y,
-        end_z);
-    EXPECT_NEAR(local[0], 0.0_rt, 1.0e-8_rt);
-    EXPECT_NEAR(local[1], 0.0_rt, 1.0e-8_rt);
-    EXPECT_NEAR(local[2], 0.0_rt, 1.0e-8_rt);
+    // Test translation
+    auto translate = amr_wind::channelbuilder::transform_to_local_coordinates(
+        0.0_rt, 0.0_rt, 0.0_rt, start[0], start[1], start[2], end[0], end[1],
+        end[2]);
+    EXPECT_NEAR(translate[0], -2.0_rt, 1.0e-8_rt);
+    EXPECT_NEAR(translate[1], -3.0_rt, 1.0e-8_rt);
+    EXPECT_NEAR(translate[2], -5.0_rt, 1.0e-8_rt);
 
-    // Point at segment midpoint should have xloc near 5
-    local = amr_wind::channelbuilder::transform_to_local_coordinates(
-        5.0_rt, 0.0_rt, 0.0_rt, start_x, start_y, start_z, end_x, end_y,
-        end_z);
-    EXPECT_NEAR(local[0], 5.0_rt, 1.0e-8_rt);
-    EXPECT_NEAR(local[1], 0.0_rt, 1.0e-8_rt);
-    EXPECT_NEAR(local[2], 0.0_rt, 1.0e-8_rt);
+    // Test rotation
+    end[0] = 7.0_rt;
+    end[1] = 8.0_rt;
+    end[2] = 10.0_rt;
 
-    // Point offset in y should have yloc with offset value
-    local = amr_wind::channelbuilder::transform_to_local_coordinates(
-        5.0_rt, 2.0_rt, 0.0_rt, start_x, start_y, start_z, end_x, end_y,
-        end_z);
-    EXPECT_NEAR(local[0], 5.0_rt, 1.0e-8_rt);
-    EXPECT_NEAR(local[1], 2.0_rt, 1.0e-8_rt);
-    EXPECT_NEAR(local[2], 0.0_rt, 1.0e-8_rt);
+    auto rotate = amr_wind::channelbuilder::transform_to_local_coordinates(
+        end[0], end[1], end[2], start[0], start[1], start[2], end[0], end[1],
+        end[2]);
+    EXPECT_NEAR(rotate[0], std::sqrt(75.0_rt), 1.0e-8_rt);
+    EXPECT_NEAR(rotate[1], 0.0_rt, 1.0e-8_rt);
+    EXPECT_NEAR(rotate[2], 0.0_rt, 1.0e-8_rt);
 
-    // Point offset in z should have zloc with offset value
-    local = amr_wind::channelbuilder::transform_to_local_coordinates(
-        5.0_rt, 0.0_rt, 3.0_rt, start_x, start_y, start_z, end_x, end_y,
-        end_z);
-    EXPECT_NEAR(local[0], 5.0_rt, 1.0e-8_rt);
-    EXPECT_NEAR(local[1], 0.0_rt, 1.0e-8_rt);
-    EXPECT_NEAR(local[2], 3.0_rt, 1.0e-8_rt);
-}
+    // Test both in 2D - xy
+    end[2] = 5.0_rt;
 
-TEST(ChannelBuilderShapes, transform_to_local_coordinates_diagonal)
-{
-    // Define a diagonal segment from (0, 0, 0) to (10, 10, 0)
-    // This tests rotation in the xy plane
-    const amrex::Real start_x = 0.0_rt;
-    const amrex::Real start_y = 0.0_rt;
-    const amrex::Real start_z = 0.0_rt;
-    const amrex::Real end_x = 10.0_rt;
-    const amrex::Real end_y = 10.0_rt;
-    const amrex::Real end_z = 0.0_rt;
+    auto both_xy = amr_wind::channelbuilder::transform_to_local_coordinates(
+        2.0_rt + 2.0_rt / sqrt(2.0_rt), 3.0_rt, 5.0_rt, start[0], start[1], start[2], end[0], end[1],
+        end[2]);
+    EXPECT_NEAR(both_xy[0], 1.0_rt, 1.0e-8_rt);
+    EXPECT_NEAR(both_xy[1], -1.0_rt, 1.0e-8_rt);
+    EXPECT_NEAR(both_xy[2], 0.0_rt, 1.0e-8_rt);
 
-    // Point at segment start
-    auto local = amr_wind::channelbuilder::transform_to_local_coordinates(
-        0.0_rt, 0.0_rt, 0.0_rt, start_x, start_y, start_z, end_x, end_y,
-        end_z);
-    EXPECT_NEAR(local[0], 0.0_rt, 1.0e-8_rt);
-    EXPECT_NEAR(local[1], 0.0_rt, 1.0e-8_rt);
-    EXPECT_NEAR(local[2], 0.0_rt, 1.0e-8_rt);
+    // Test both in 2D - xz
+    end[1] = 3.0_rt;
+    end[2] = 10.0_rt;
 
-    // Point at segment in the direction of (end - start)
-    // Should have xloc > 0 and yloc ~= 0
-    local = amr_wind::channelbuilder::transform_to_local_coordinates(
-        7.071067812_rt, 7.071067812_rt, 0.0_rt, start_x, start_y, start_z,
-        end_x, end_y, end_z);
-    EXPECT_NEAR(local[0], 10.0_rt, 1.0e-5_rt);
-    EXPECT_NEAR(local[1], 0.0_rt, 1.0e-5_rt);
-    EXPECT_NEAR(local[2], 0.0_rt, 1.0e-5_rt);
+    auto both_xz = amr_wind::channelbuilder::transform_to_local_coordinates(
+        2.0_rt + 2.0_rt / sqrt(2.0_rt), 3.0_rt, 5.0_rt, start[0], start[1], start[2], end[0], end[1],
+        end[2]);
+    EXPECT_NEAR(both_xz[0], 1.0_rt, 1.0e-8_rt);
+    EXPECT_NEAR(both_xz[1], 0.0_rt, 1.0e-8_rt);
+    EXPECT_NEAR(both_xz[2], -1.0_rt, 1.0e-8_rt);
+
 }
 
 } // namespace amr_wind_tests
