172ce2403272092df3188d913a8de3bbd5785e63
Michael Kuhn <mkuhn@nlr.gov>
2026-04-23 16:01:39 -0600

    fix references within unit test

--- unit_tests/multiphase/test_channel_builder_shapes.cpp
+++ unit_tests/multiphase/test_channel_builder_shapes.cpp
@@ -1,10 +1,10 @@
-#include "aw_test_utils/AmrexTest.H"
-#include "amr-wind/physics/multiphase/ChannelBuilder.H"
+#include "ks_test_utils/AmrexTest.H"
+#include "src/physics/multiphase/ChannelBuilder.H"
 #include "AMReX_REAL.H"
 
 using namespace amrex::literals;
 
-namespace amr_wind_tests {
+namespace kynema_sgf_tests {
 
 TEST(ChannelBuilderShapes, trapezoid_inside_outside)
 {
@@ -14,23 +14,23 @@ TEST(ChannelBuilderShapes, trapezoid_inside_outside)
 
     // Interior points
     EXPECT_TRUE(
-        amr_wind::channelbuilder::trapezoid(
+        kynema_sgf::channelbuilder::trapezoid(
             top, bottom, height, 0.0_rt, 0.0_rt));
     EXPECT_TRUE(
-        amr_wind::channelbuilder::trapezoid(
+        kynema_sgf::channelbuilder::trapezoid(
             top, bottom, height, 0.8_rt, 1.5_rt));
 
     // Boundary point should be included
     EXPECT_TRUE(
-        amr_wind::channelbuilder::trapezoid(
+        kynema_sgf::channelbuilder::trapezoid(
             top, bottom, height, -1.5_rt, 3.0_rt));
 
     // Exterior points
     EXPECT_FALSE(
-        amr_wind::channelbuilder::trapezoid(
+        kynema_sgf::channelbuilder::trapezoid(
             top, bottom, height, 3.1_rt, 0.0_rt));
     EXPECT_FALSE(
-        amr_wind::channelbuilder::trapezoid(
+        kynema_sgf::channelbuilder::trapezoid(
             top, bottom, height, 0.0_rt, 3.1_rt));
 }
 
@@ -41,19 +41,19 @@ TEST(ChannelBuilderShapes, ellipse_inside_outside)
 
     // Interior points
     EXPECT_TRUE(
-        amr_wind::channelbuilder::ellipse(ax_horz, ax_vert, 0.0_rt, 0.0_rt));
+        kynema_sgf::channelbuilder::ellipse(ax_horz, ax_vert, 0.0_rt, 0.0_rt));
     EXPECT_TRUE(
-        amr_wind::channelbuilder::ellipse(ax_horz, ax_vert, 2.0_rt, 0.5_rt));
+        kynema_sgf::channelbuilder::ellipse(ax_horz, ax_vert, 2.0_rt, 0.5_rt));
 
     // Boundary point should be included
     EXPECT_TRUE(
-        amr_wind::channelbuilder::ellipse(ax_horz, ax_vert, 3.0_rt, 0.0_rt));
+        kynema_sgf::channelbuilder::ellipse(ax_horz, ax_vert, 3.0_rt, 0.0_rt));
 
     // Exterior points
     EXPECT_FALSE(
-        amr_wind::channelbuilder::ellipse(ax_horz, ax_vert, 3.2_rt, 0.0_rt));
+        kynema_sgf::channelbuilder::ellipse(ax_horz, ax_vert, 3.2_rt, 0.0_rt));
     EXPECT_FALSE(
-        amr_wind::channelbuilder::ellipse(ax_horz, ax_vert, 2.5_rt, 1.5_rt));
+        kynema_sgf::channelbuilder::ellipse(ax_horz, ax_vert, 2.5_rt, 1.5_rt));
 }
 
 TEST(ChannelBuilderShapes, is_point_within_planes)
@@ -68,27 +68,27 @@ TEST(ChannelBuilderShapes, is_point_within_planes)
 
     // Point between planes should return true
     EXPECT_TRUE(
-        amr_wind::channelbuilder::is_point_within_planes(
+        kynema_sgf::channelbuilder::is_point_within_planes(
             5.0_rt, 0.0_rt, 0.0_rt, start_x, start_y, start_z, end_x, end_y,
             end_z));
 
     // Points on or near segment should return true
     EXPECT_TRUE(
-        amr_wind::channelbuilder::is_point_within_planes(
+        kynema_sgf::channelbuilder::is_point_within_planes(
             0.0_rt, 0.0_rt, 0.0_rt, start_x, start_y, start_z, end_x, end_y,
             end_z));
     EXPECT_TRUE(
-        amr_wind::channelbuilder::is_point_within_planes(
+        kynema_sgf::channelbuilder::is_point_within_planes(
             10.0_rt, 0.0_rt, 0.0_rt, start_x, start_y, start_z, end_x, end_y,
             end_z));
 
     // Points outside segment planes should return false
     EXPECT_FALSE(
-        amr_wind::channelbuilder::is_point_within_planes(
+        kynema_sgf::channelbuilder::is_point_within_planes(
             -5.0_rt, 0.0_rt, 0.0_rt, start_x, start_y, start_z, end_x, end_y,
             end_z));
     EXPECT_FALSE(
-        amr_wind::channelbuilder::is_point_within_planes(
+        kynema_sgf::channelbuilder::is_point_within_planes(
             15.0_rt, 0.0_rt, 0.0_rt, start_x, start_y, start_z, end_x, end_y,
             end_z));
 }
@@ -109,7 +109,7 @@ TEST(ChannelBuilderShapes, interpolate_to_get_local_dimensions)
     const amrex::Real dim1_e = 5.0_rt;
     const amrex::Real dim2_e = 6.0_rt;
 
-    auto local_dims = amr_wind::channelbuilder::get_local_dimensions(
+    auto local_dims = kynema_sgf::channelbuilder::get_local_dimensions(
         5.0_rt, 4.0_rt, 3.0_rt, start_x, start_y, start_z, end_x, end_y, end_z,
         dim0_s, dim1_s, dim2_s, dim0_e, dim1_e, dim2_e);
     EXPECT_NEAR(local_dims[0], 2.5_rt, 1e-8_rt);
@@ -123,7 +123,7 @@ TEST(ChannelBuilderShapes, transform_to_local_coordinates)
     amrex::Vector<amrex::Real> end{10.0_rt, 3.0_rt, 5.0_rt};
 
     // Test translation
-    auto translate = amr_wind::channelbuilder::transform_to_local_coordinates(
+    auto translate = kynema_sgf::channelbuilder::transform_to_local_coordinates(
         0.0_rt, 0.0_rt, 0.0_rt, start[0], start[1], start[2], end[0], end[1],
         end[2]);
     EXPECT_NEAR(translate[0], -2.0_rt, 1.0e-8_rt);
@@ -135,7 +135,7 @@ TEST(ChannelBuilderShapes, transform_to_local_coordinates)
     end[1] = 8.0_rt;
     end[2] = 10.0_rt;
 
-    auto rotate = amr_wind::channelbuilder::transform_to_local_coordinates(
+    auto rotate = kynema_sgf::channelbuilder::transform_to_local_coordinates(
         end[0], end[1], end[2], start[0], start[1], start[2], end[0], end[1],
         end[2]);
     EXPECT_NEAR(rotate[0], std::sqrt(75.0_rt), 1.0e-8_rt);
@@ -145,7 +145,7 @@ TEST(ChannelBuilderShapes, transform_to_local_coordinates)
     // Test both in 2D - xy
     end[2] = 5.0_rt;
 
-    auto both_xy = amr_wind::channelbuilder::transform_to_local_coordinates(
+    auto both_xy = kynema_sgf::channelbuilder::transform_to_local_coordinates(
         2.0_rt + 2.0_rt / sqrt(2.0_rt), 3.0_rt, 5.0_rt, start[0], start[1],
         start[2], end[0], end[1], end[2]);
     EXPECT_NEAR(both_xy[0], 1.0_rt, 1.0e-8_rt);
@@ -156,7 +156,7 @@ TEST(ChannelBuilderShapes, transform_to_local_coordinates)
     end[1] = 3.0_rt;
     end[2] = 10.0_rt;
 
-    auto both_xz = amr_wind::channelbuilder::transform_to_local_coordinates(
+    auto both_xz = kynema_sgf::channelbuilder::transform_to_local_coordinates(
         2.0_rt + 2.0_rt / sqrt(2.0_rt), 3.0_rt, 5.0_rt, start[0], start[1],
         start[2], end[0], end[1], end[2]);
     EXPECT_NEAR(both_xz[0], 1.0_rt, 1.0e-8_rt);
@@ -164,4 +164,4 @@ TEST(ChannelBuilderShapes, transform_to_local_coordinates)
     EXPECT_NEAR(both_xz[2], -1.0_rt, 1.0e-8_rt);
 }
 
-} // namespace amr_wind_tests
+} // namespace kynema_sgf_tests
