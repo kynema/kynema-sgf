f8947d48a62da2c8de930131349b2c23f7dc82aa
Michael Kuhn <mkuhn@nlr.gov>
2026-05-22 16:39:47 -0600

    use "toLower" to prevent dumb conflicts

--- src/physics/multiphase/ChannelBuilder.cpp
+++ src/physics/multiphase/ChannelBuilder.cpp
@@ -212,7 +212,7 @@ ChannelBuilder::ChannelBuilder(CFDSim& sim)
 
         std::string stype = "Ellipse";
         pp1.query("type", stype);
-        if (stype == "Ellipse") {
+        if (amrex::toLower(stype) == "ellipse") {
             h_type.emplace_back(ChannelSegmentType::Ellipse);
             if (pp1.contains("diameter")) {
                 pp1.get("diameter", dim0_s);
@@ -235,7 +235,7 @@ ChannelBuilder::ChannelBuilder(CFDSim& sim)
                 pp1.get("horizontal_axis_end", dim0_e);
                 pp1.get("vertical_axis_end", dim1_e);
             }
-        } else if (stype == "Trapezoid") {
+        } else if (amrex::toLower(stype) == "trapezoid") {
             h_type.emplace_back(ChannelSegmentType::Trapezoid);
             if (pp1.contains("top_width")) {
                 pp1.get("top_width", dim0_s);
@@ -272,18 +272,19 @@ ChannelBuilder::ChannelBuilder(CFDSim& sim)
         h_segment_end.emplace_back(
             amrex::Array<amrex::Real, 3>{seg_end[0], seg_end[1], seg_end[2]});
 
-        stype = "Uniform";
+        stype = "uniform";
         pp1.query("velocity_profile", stype);
-        if (stype == "Uniform") {
+        stype = amrex::toLower(stype);
+        if (stype == "uniform") {
             h_velocity_profile.emplace_back(ChannelVelocityProfile::Uniform);
-        } else if (stype == "Linear") {
+        } else if (stype == "linear") {
             h_velocity_profile.emplace_back(ChannelVelocityProfile::Linear);
-        } else if (stype == "Parabolic") {
+        } else if (stype == "parabolic") {
             h_velocity_profile.emplace_back(ChannelVelocityProfile::Parabolic);
         } else {
             amrex::Abort(
                 "Invalid channel velocity profile specified: " + stype +
-                ". Only 'Uniform', 'Linear', and 'Parabolic' are supported.");
+                ". Only 'uniform', 'linear', and 'parabolic' are supported.");
         }
         amrex::Real flow_speed = 0.0_rt;
         pp1.get("flow_speed", flow_speed);
