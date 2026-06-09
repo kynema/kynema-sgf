3fda601f067b453dec855c480abbcc3d22b04dc4
Michael Kuhn <mkuhn@nlr.gov>
2026-06-09 14:50:33 -0600

    add second subvolume with coarser refinements; fails

--- unit_tests/utilities/test_subvolume.cpp
+++ unit_tests/utilities/test_subvolume.cpp
@@ -94,41 +94,87 @@ TEST_F(SubvolumeTest, rectangular_subvolume_output)
         pp.add("type", std::string("Rectangular"));
         pp.addarr("origin", amrex::Vector<amrex::Real>{0.0_rt, 0.0_rt, 0.0_rt});
         pp.addarr("num_points", amrex::Vector<int>{4, 4, 4});
-        pp.add("dx", 4.0_rt);
+        pp.addarr(
+            "dx_vec", amrex::Vector<amrex::Real>{8.0_rt, 16.0_rt, 12.0_rt});
     }
 
     kynema_sgf::subvolume::Subvolume subvol(sim(), "subvolume");
     subvol.initialize();
     subvol.output_actions();
 
-    const std::filesystem::path header_path(
-        "post_processing/subvolume_chunk100000/Header");
-    EXPECT_TRUE(std::filesystem::exists(header_path));
+    {
 
-    amrex::PlotFileData pf("post_processing/subvolume_chunk100000");
-    EXPECT_EQ(pf.finestLevel(), 0);
-    EXPECT_EQ(pf.nComp(), 1);
-    ASSERT_EQ(pf.varNames().size(), 1);
-    EXPECT_EQ(pf.varNames()[0], "density");
+        const std::filesystem::path header_path(
+            "post_processing/subvolume_chunk100000/Header");
+        EXPECT_TRUE(std::filesystem::exists(header_path));
 
-    auto mf_out = pf.get(0, "density");
-    for (amrex::MFIter mfi(mf_out); mfi.isValid(); ++mfi) {
-        const auto bx = mfi.validbox();
-        const auto arr = mf_out.const_array(mfi);
-        const auto lo = amrex::lbound(bx);
-        const auto hi = amrex::ubound(bx);
+        amrex::PlotFileData pf("post_processing/subvolume_chunk100000");
+        EXPECT_EQ(pf.finestLevel(), 0);
+        EXPECT_EQ(pf.nComp(), 1);
+        ASSERT_EQ(pf.varNames().size(), 1);
+        EXPECT_EQ(pf.varNames()[0], "density");
 
-        for (int k = lo.z; k <= hi.z; ++k) {
-            for (int j = lo.y; j <= hi.y; ++j) {
-                for (int i = lo.x; i <= hi.x; ++i) {
-                    const amrex::Real dx_sv = 4.0_rt;
-                    const amrex::Real dx_dom = 128.0_rt / 32.0_rt;
-                    const amrex::Real expected =
-                        dx_sv * static_cast<amrex::Real>(i + j + k) +
-                        3.0_rt * 0.5_rt * dx_dom;
-                    EXPECT_NEAR(
-                        arr(i, j, k), expected,
-                        std::numeric_limits<amrex::Real>::epsilon() * 1.0e3_rt);
+        auto mf_out = pf.get(0, "density");
+        for (amrex::MFIter mfi(mf_out); mfi.isValid(); ++mfi) {
+            const auto bx = mfi.validbox();
+            const auto arr = mf_out.const_array(mfi);
+            const auto lo = amrex::lbound(bx);
+            const auto hi = amrex::ubound(bx);
+
+            for (int k = lo.z; k <= hi.z; ++k) {
+                for (int j = lo.y; j <= hi.y; ++j) {
+                    for (int i = lo.x; i <= hi.x; ++i) {
+                        const amrex::Real dx_sv = 4.0_rt;
+                        const amrex::Real dx_dom = 128.0_rt / 32.0_rt;
+                        const amrex::Real expected =
+                            dx_sv * static_cast<amrex::Real>(i + j + k) +
+                            3.0_rt * 0.5_rt * dx_dom;
+                        EXPECT_NEAR(
+                            arr(i, j, k), expected,
+                            std::numeric_limits<amrex::Real>::epsilon() *
+                                1.0e3_rt);
+                    }
+                }
+            }
+        }
+    }
+
+    {
+
+        const std::filesystem::path header_path(
+            "post_processing/subvolume_chunk200000/Header");
+        EXPECT_TRUE(std::filesystem::exists(header_path));
+
+        amrex::PlotFileData pf("post_processing/subvolume_chunk200000");
+        EXPECT_EQ(pf.finestLevel(), 0);
+        EXPECT_EQ(pf.nComp(), 1);
+        ASSERT_EQ(pf.varNames().size(), 1);
+        EXPECT_EQ(pf.varNames()[0], "density");
+
+        auto mf_out = pf.get(0, "density");
+        for (amrex::MFIter mfi(mf_out); mfi.isValid(); ++mfi) {
+            const auto bx = mfi.validbox();
+            const auto arr = mf_out.const_array(mfi);
+            const auto lo = amrex::lbound(bx);
+            const auto hi = amrex::ubound(bx);
+
+            for (int k = lo.z; k <= hi.z; ++k) {
+                for (int j = lo.y; j <= hi.y; ++j) {
+                    for (int i = lo.x; i <= hi.x; ++i) {
+                        const amrex::Real dx_sv = 8.0_rt;
+                        const amrex::Real dy_sv = 16.0_rt;
+                        const amrex::Real dz_sv = 12.0_rt;
+                        const amrex::Real dx_dom = 128.0_rt / 32.0_rt;
+                        const amrex::Real expected =
+                            dx_sv * static_cast<amrex::Real>(i) +
+                            dy_sv * static_cast<amrex::Real>(j) +
+                            dz_sv * static_cast<amrex::Real>(k) +
+                            3.0_rt * 0.5_rt * dx_dom;
+                        EXPECT_NEAR(
+                            arr(i, j, k), expected,
+                            std::numeric_limits<amrex::Real>::epsilon() *
+                                1.0e3_rt);
+                    }
                 }
             }
         }
