7d80d875da4fabe0676f65aaed5bb03b4857c20b
mbkuhn <mkuhn@nlr.gov>
2026-07-23 16:39:05 -0600

    start of template with ability to get target field

--- src/equation_systems/source_terms/DampingLayerSource.cpp
+++ src/equation_systems/source_terms/DampingLayerSource.cpp
@@ -4,6 +4,9 @@ namespace kynema_sgf::pde {
 
 template class DampingLayerSource<MomentumSource>;
 template class DampingLayerSource<TemperatureSource>;
+template class DampingLayerSource<DensitySource>;
+template class DampingLayerSource<TKESource>;
+template class DampingLayerSource<SDRSource>;
 template class DampingLayerSource<SourceTerm>;
 
 } // namespace kynema_sgf::pde
