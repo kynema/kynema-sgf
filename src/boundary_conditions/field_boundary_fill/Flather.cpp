a792f674057744db51bef051d83b60c52f0c15b2
Michael Kuhn <mkuhn@nlr.gov>
2026-09-14 15:05:56 -0600

    also prevent rapid growth

--- src/boundary_conditions/field_boundary_fill/Flather.cpp
+++ src/boundary_conditions/field_boundary_fill/Flather.cpp
@@ -578,7 +578,10 @@ void Flather::set_velocity(
                 // Apply scale to velocity, depends on direction
                 if (prescribed_inflow || override_interior) {
                     local_vel = arr(iv, fcomp);
-                    scaled_vel = local_vel * (Flather_val / boundary_val);
+                    auto scale_exterior = (Flather_val / boundary_val);
+                    scale_exterior = amrex::min<amrex::Real>(scale_exterior, vscale_max);
+                    // minimum scale is 0, enforced by sign guard
+                    scaled_vel = local_vel * scale_exterior;
                 } else {
                     local_vel = local_internal_vel;
                     scaled_vel = local_vel * scale_interior;
